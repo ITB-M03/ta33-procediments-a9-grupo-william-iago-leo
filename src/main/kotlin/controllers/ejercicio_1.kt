d776c6d6ae05abfeb650b9c13015c05c3966a9d5
IagoMacBook <iagomzzh@gmail.com>
2024-12-13 12:58:09 +0100

    A punto de acabar el ejercicio de la maquina de billetes, solo faltan ultimos detalles

--- src/main/kotlin/controllers/ejercicio_1.kt
+++ src/main/kotlin/controllers/ejercicio_1.kt
@@ -1,22 +1,24 @@
 package org.example.controllers
 
-import java.math.BigDecimal
-import java.math.RoundingMode
 import java.text.DecimalFormat
-import java.util.InputMismatchException
 import java.util.Scanner
-import kotlin.math.abs
 
 fun main() {
+    repeticion()
+
+}
+
+fun repeticion(){
+
     val scan = Scanner(System.`in`)
-    val eleccion = menuBitlletsIEleccio(scan)
+    val eleccion = eleccioTipusBitllet(scan)
     val zonaElegida = triarZona(scan)
     val precio = gestionarEleccion(eleccion, zonaElegida)
     imprimirEleccion(eleccionEnString(eleccion), zonaElegida, precio)
     pagarITornarCanvi(scan, precio)
+    tiquet(scan, eleccionEnString(eleccion), zonaElegida, precio)
 
 }
-
 fun gestionarEleccion(menuBitlletsIEleccio: Int,eleccioZona:Int): String {
     var precio = 0.0
     var eleccionEnString = " "
@@ -31,56 +33,47 @@ fun gestionarEleccion(menuBitlletsIEleccio: Int,eleccioZona:Int): String {
         1 -> precio = precio * 1
         2 -> precio = precio * 1.3125
         3 -> precio = precio * 1.8443
-
-
     }
     val formato = DecimalFormat("#.00") // Si usas otro sistema operativo usa la coma en ve del putno
     val numeroFormateado = formato.format(precio).toString()
-
     return numeroFormateado
-
 }
-
-
-fun menuBitlletsIEleccio(scan:Scanner):Int{
-    println("""
-        - - - - - - - - - - - - - - - - - - - - - - 
-        Quin bitllet desitja adquirir?
-        1. Btillet Senzill
-        2. TCasual
-        3. TUsual
-        4. TFamiliar
-        5. TJove
-    """.trimIndent())
-    try {
-        val eleccio = scan.nextInt()
-
-    }
-    catch (e:InputMismatchException) {
-        println("")
-    }
-
-    while ( eleccio < 0 || eleccio > 5 ) {
-        println("Has de triar alguna opció valida (1-5)")
-        val eleccio = scan.nextInt()
-
-
+fun eleccioTipusBitllet(scan:Scanner):Int{
+   imprimirMenuEleccioTipusBitllet()
+    var eleccio = 0
+    while (eleccio == 0) {
+        try {
+            eleccio = scan.nextInt()
+            while (eleccio < 1 || eleccio > 5) {
+                println("Has de triar alguna opció vàlida (1-5)")
+                eleccio = scan.nextInt()
+            }
+        } catch (e: Exception) {
+            println("Has d'introduir un número del 1-5")
+            eleccio = 0
+            scan.next()
+        }
     }
     return eleccio
-
 }
-
 fun triarZona(scan: Scanner):Int{
-    println("""
-        De quina zona desitja adquirir el bitllet?
-        1
-        2
-        3
-    """.trimIndent())
-    val eleccio = scan.nextInt()
+    imprimirMenuEleccioZona()
+    var eleccio = 0
+    while (eleccio == 0){
+        try {
+            eleccio = scan.nextInt()
+            while (eleccio < 1 || eleccio > 3) {
+                println("Has de triar alguna opció vàlida (1-3)")
+                eleccio = scan.nextInt()
+            }
+        } catch (e: Exception) {
+            println("Has d'introduir un número del 1-3")
+            eleccio = 0
+            scan.next()
+        }
+    }
     return  eleccio
 }
-
 fun eleccionEnString(menuBitlletsIEleccio: Int):String{
     var eleccionEnString = " "
     when (menuBitlletsIEleccio){
@@ -92,47 +85,101 @@ fun eleccionEnString(menuBitlletsIEleccio: Int):String{
     }
     return eleccionEnString
 }
-
-fun imprimirEleccion(eleccionString:String, zona:Int, precio:String){
+fun imprimirEleccion(eleccionString:String, zona:Int, precio:String):String{
     println("Has escollit l'opcio $eleccionString, zona $zona")
     println("El preu del bitllet es de $precio€")
-
-
-
+    return eleccionString
 }
-
-
 fun pagarITornarCanvi(scan:Scanner, precio:String){
+    val formato = DecimalFormat("#.00")
+    println("Introdueix moneda per moneda")
+    var dineroMetidoPorElCliente = 0.0
+    var aPagar = 0.0
+    imprimirmonedesPermeses()
+    while (aPagar == 0.0)
+    try { aPagar = precio.toDouble() }
+    catch (e:Exception){
+        println("Has d'introduir monedes!, (numeros)")
+        aPagar = 0.0
+        scan.next()
+    }
+    while (dineroMetidoPorElCliente < aPagar) {
+        var dineroFormateado = formato.format(aPagar - dineroMetidoPorElCliente)
+        println("Et queda per pagar $dineroFormateado€")
+        var introducido = 0.0
+        while (introducido == 0.0) {
+            try { introducido = scan.nextDouble() }
+            catch (e: Exception) {
+                println("Has d'introduir monedes!, (numeros)")
+                introducido = 0.0
+                scan.next()}}
+        if (introducido != 0.05 && introducido != 0.10 && introducido != 0.20 && introducido != 0.50 && introducido != 1.0 && introducido != 2.0 && introducido != 5.0 && introducido != 10.0 && introducido != 20.0 && introducido != 50.0) {
+            println("Moneda no vàlida, introdueix una altra moneda.")
+        } else { dineroMetidoPorElCliente += introducido }}
+    if (aPagar > 0.0) {
+        var dineroFormateado1 = formato.format(dineroMetidoPorElCliente - aPagar)
+        println("Et torno el canvi $dineroFormateado1€")
+        animacioTornarCanvi()
+    }
+}
+fun imprimirMenuEleccioTipusBitllet(){
+    println("""
+        - - - - - - - - - - - - - - - - - - - - - - 
+        Quin bitllet desitja adquirir?
+        1. Btillet Senzill
+        2. TCasual
+        3. TUsual
+        4. TFamiliar
+        5. TJove
+    """.trimIndent())
+}
+fun imprimirMenuEleccioZona(){
+    println("""
+        De quina zona desitja adquirir el bitllet?
+        1
+        2
+        3
+    """.trimIndent())
+}
+fun imprimirmonedesPermeses(){
     println("""
         PAGAMENT EN EFECTIU:
         MONEDES I BITLLETS PERMESOS: 
         0.05€, 0.10€, 0.20€, 0.50€, 1€, 2€, 5€, 10€, 20€ i 50€.
-        
-       et queda per pagar: $precio€
     """.trimIndent())
-
-    println("Introdueix moneda per moneda")
-    var dineroMetidoPorElCliente = 0.0
-    val aPagar = precio.toDouble()
-
-    while (dineroMetidoPorElCliente < aPagar){
-        println("Introdueix")
-        println("Et queda per pagar ${aPagar - dineroMetidoPorElCliente}")
-        var introducido = scan.nextDouble()
-        if (introducido != 0.05 && introducido != 0.10 && introducido != 0.20 && introducido != 0.50 && introducido != 1.0 && introducido != 2.0 && introducido != 5.0 && introducido != 10.0 && introducido != 20.0 && introducido != 50.0) {
-            println("Moneda no vàlida, introdueix una altra moneda.")
-        } else {
-            dineroMetidoPorElCliente += introducido
-        }
-    }
-
-    if (aPagar > 0.0){
-       println("Et torno el canvi ${dineroMetidoPorElCliente - aPagar}")
-    }
-
-
-
-
-
 }
+fun animacioTornarCanvi(){
+    Thread.sleep(500)
+    println("Cling")
+    Thread.sleep(500)
+    println("Clong")
+    Thread.sleep(500)
+    println("Cling")
+    Thread.sleep(500)
+    println("Ja pots recollir els diners")
+}
+
+fun tiquet(scan: Scanner, eleccionString: String, zonaTriada:Int, precio: String){
+    println("Vol tiquet? S/N ")
+    val eleccio = scan.next().lowercase()
+    if (eleccio == "s"){
+        println("""
+            - - - - - - - - - - - - - - - - - -
+                          TICKET
+            - - - - - - - - - - - - - - - - - -            
+              BITLLET: $eleccionString
+              ZONA/ES: $zonaTriada
+              PREU: $precio
+              METODE DE PAGAMENT: EFECTIU
+             
+             **********************************
+             *** GRÀCIES PER LA SEVA COMPRA ***
+             **********************************
+             
+            - - - - - - - - - - - - - - - - - -  
+                           TMB
+            - - - - - - - - - - - - - - - - - -                 
+        """.trimIndent())}
+}
+
 
